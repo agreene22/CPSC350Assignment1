341ce19c122e59aa35848a9d8ef8882e36dc88e3
Anna Greene <agreene@chapman.edu>
2020-02-15 00:19:39 +0000

    varaince calculation

--- main.cpp
+++ main.cpp
@@ -15,14 +15,15 @@ int main(int argc, char **argv){
     string fileName = argv[1];
 
     while (input == "y"){
-      int lineCount = 0;
+      float lineCount = 0.0;
       int nucleotideCount = 0;
-      int totalNucleotides = 0;
+      float totalNucleotides = 0.0;
       int aCount = 0;
       int tCount = 0;
       int gCount = 0;
       int cCount = 0;
-      int variance = 0;
+      double variance = 0.0;
+      double mean = 0.0;
       double c = 0.0;
       double d = 0.0;
 
@@ -30,35 +31,38 @@ int main(int argc, char **argv){
 
       while(!inFS.eof()){
         inFS >> dnaSequence;
-        lineCount++;
-        cout << dnaSequence << endl; //WHY IS IT DOING THE LAST LINE OF THE FILE TWICE????
-        cout << lineCount << endl;
-        nucleotideCount = 0;
-        for(int i = 0; i < dnaSequence.size(); ++i){
-          char currNucleotide = toupper(dnaSequence[i]);
-          nucleotideCount++;
-          switch (currNucleotide) {
-            case 'A':
-            aCount++;
-            cout << aCount << " "; // Remove these print statements later
-            break;
-            case 'C':
-            cCount++;
-            cout << cCount << " ";
-            break;case 'T':
-            tCount++;
-            cout << tCount << " ";
-            break;
-            case 'G':
-            gCount++;
-            cout << gCount << " ";
+        if(!inFS.fail()){
+          lineCount++;
+          cout << dnaSequence << endl; //WHY IS IT DOING THE LAST LINE OF THE FILE TWICE????
+          cout << lineCount << endl;
+          nucleotideCount = 0;
+          for(int i = 0; i < dnaSequence.size(); ++i){
+            char currNucleotide = toupper(dnaSequence[i]);
+            nucleotideCount++;
+            switch (currNucleotide) {
+              case 'A':
+              aCount++;
+              cout << aCount << " "; // Remove these print statements later
+              break;
+              case 'C':
+              cCount++;
+              cout << cCount << " ";
+              break;case 'T':
+              tCount++;
+              cout << tCount << " ";
+              break;
+              case 'G':
+              gCount++;
+              cout << gCount << " ";
+            }
+            cout << currNucleotide << endl; // Remove this later
           }
-          cout << currNucleotide << endl; // Remove this later
+          totalNucleotides += nucleotideCount;
         }
-        totalNucleotides += nucleotideCount;
         cout << "total " << totalNucleotides << endl;
       }
 
+      inFS.close();
       outFS.open("annagreene.out");
 
       // compute sum, mean, variance, standard deviation
@@ -68,15 +72,32 @@ int main(int argc, char **argv){
       outFS << "Assignment 1" << endl;
       outFS << endl;
 
-      // variance = ((each length - mean)^2 all added together)/lineCount
+      cout << totalNucleotides << " " << lineCount << endl;
+      mean = totalNucleotides/lineCount;
 
       outFS << "sum: " << totalNucleotides << endl;
-      outFS << "mean: " << (totalNucleotides/lineCount) << endl;
+      outFS << "mean: " << mean << endl;
+
+      inFS.open(fileName);
+
+      while(!inFS.eof()){
+        inFS >> dnaSequence;
+        if(!inFS.fail()){
+          float size = dnaSequence.size() - mean;
+          variance += pow(size, 2.0);
+          // cout << variance << endl;
+        }
+      }
+      inFS.close();
+
+      // variance = ((each length - mean)^2 all added together)/lineCount
+      // cout << lineCount << " ";
+      variance /= (lineCount-1); //NOT WORKING, I ASSUME BECAUSE OF FLOAT/INT ERROR
+
       outFS << "variance: " << variance << endl;
 
-      inFS.close();
       outFS.close();
-      
+
       cout << "Would you like to process another list? (y/n)" << endl;
       cin >> input;
       if(input == "n"){
